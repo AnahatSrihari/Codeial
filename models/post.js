67adcbbf4f6b883db95399b96cfd58cd096bcd81
AnahatSrihari <anahatsrihari@gmail.com>
2023-05-27 15:59:20 +0530

    Creating Schema for Posts

---
+++ models/post.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// Creating Schema for the posts
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        //referring to the user by using the unique object-id
+        type:  mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
+    }
+},{
+    // it creates 2 fields created-at AND  updated-at
+    timestamps: true
+});
+
+// tell that it is a model in the DB
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
