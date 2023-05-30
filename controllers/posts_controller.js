f4abb793a7c7ae424a50263bbeb01deca941ac06
AnahatSrihari <anahatsrihari@gmail.com>
2023-05-30 11:02:10 +0530

    Deleting a Post (Authorized)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,7 +2,10 @@
 // return res.end('<h1> User Post - Controller </h1>');
 //}
 
-const Post = require('../models/post')
+const Post = require('../models/post');
+// Importing the comments form models to check and delete it
+const Comment = require('../models/comment');
+
 
 module.exports.create = function(req, res){
     Post.create({
@@ -12,5 +15,23 @@ module.exports.create = function(req, res){
         if(err){console.log('error in creating a post'); return;}
 
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        //finding and checking the user is the same who posted the comment
+        // .id means converting the object id into string
+        if (post.user == req.user.id){
+            post.remove();
+            //finding and delteing the all posts of the user-id
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            // if err redirect back to same page
+            return res.redirect('back');
+        }
+
     });
 }
