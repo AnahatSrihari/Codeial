2f423cc0f68db595aa5852d945635b5eebfb7b53
AnahatSrihari <anahatsrihari@gmail.com>
2023-06-01 22:03:05 +0530

    Deleting a Post  AJAX

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,8 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
+
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -53,6 +55,25 @@
                     </div>
                     
                 </li>`)
+    }
+
+
+     // method to delete a post from DOM
+     let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+
+        });
     }
 
 
