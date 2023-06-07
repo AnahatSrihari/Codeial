29fbe08432ce734cdcdf72fe522436271aaa4879
AnahatSrihari <anahatsrihari@gmail.com>
2023-06-07 15:45:53 +0530

    M - 9 L-7 ( 7 ) Finally, Let’s Send and Receive Messages. - Chatting Engine

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -18,17 +18,55 @@ class ChatEngine{
         this.socket.on('connect', function(){
             console.log('connection established using sockets...!');
 
-            //This event is emitted (gives notification to all the users in the chat rooom)  
+
             self.socket.emit('join_room', {
                 user_email: self.userEmail,
-                chatroom: 'codeial'// name of the chat room 
+                chatroom: 'codeial'
             });
-            // event listener of emit
+
             self.socket.on('user_joined', function(data){
                 console.log('a user joined!', data);
             })
 
 
         });
+
+        // CHANGE :: send a message on clicking the send message button
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if (msg != ''){
+                self.socket.emit('send_message', {
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+            }
+        });
+
+        self.socket.on('receive_message', function(data){
+            console.log('message received', data.message);
+
+
+            let newMessage = $('<li>');
+
+            let messageType = 'other-message';
+
+            if (data.user_email == self.userEmail){
+                messageType = 'self-message';
+            }
+
+            newMessage.append($('<span>', {
+                'html': data.message
+            }));
+
+            newMessage.append($('<sub>', {
+                'html': data.user_email
+            }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-messages-list').append(newMessage);
+        })
     }
 }
